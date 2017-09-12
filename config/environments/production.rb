7d91d0788c8df8901593f6c54bb7ee6cf92504ea
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-09-12 10:32:06 +0900

    host修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'peaceful-mesa-42487.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: '52.69.143.174'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
